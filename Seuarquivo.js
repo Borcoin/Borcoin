f68b5fc349ebc29cdda0e0cf137daf54ab0fa01f
Borcoin <153922169+Borcoin@users.noreply.github.com>
2023-12-16 20:41:35 -0300

    Delete Seuarquivo.js

--- Seuarquivo.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("meuBotao").addEventListener("click", function() {
-  // Altere "outrapagina.html" para a URL da página para a qual você deseja redirecionar
-  window.location.href = "outrapagina.html";
-});
