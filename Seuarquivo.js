ed562ac5e3297fd0335969acfe5b9c29d6f833f3
Borcoin <153922169+Borcoin@users.noreply.github.com>
2023-12-16 20:33:46 -0300

    Create Seuarquivo.js

---
+++ Seuarquivo.js
@@ -0,0 +1,4 @@
+document.getElementById("meuBotao").addEventListener("click", function() {
+  // Altere "outrapagina.html" para a URL da página para a qual você deseja redirecionar
+  window.location.href = "outrapagina.html";
+});
