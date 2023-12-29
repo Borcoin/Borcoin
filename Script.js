77d5e5b38d398cc6cd23d9d62ecfa1852a85503c
Borcoin <153922169+Borcoin@users.noreply.github.com>
2023-12-29 00:02:04 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,19 @@
+let progress = 0;
+let itemCost = 10;
 
+document.getElementById('buy-btn').addEventListener('click', function() {
+    // Adicione aqui o código para verificar se o usuário tem progresso suficiente para comprar o item
+    if (progress >= itemCost) {
+        progress -= itemCost;
+        itemCost *= 2;
+        updateProgress();
+        alert('Item comprado! Progresso atualizado.');
+    } else {
+        
+    }
+});
+
+// Adicione este código para redirecionar para a página da loja quando o botão for pressionado
+document.getElementById('buy-btn').addEventListener('click', function() {
+    window.location.href = 'loja.html'; // Substitua 'loja.html' pelo nome da sua página de loja
+});
